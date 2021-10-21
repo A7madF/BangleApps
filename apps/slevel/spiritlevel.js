969fbe2bcfa329480c7ca5c452b4ad797b511209
A7madF <91619179+A7madF@users.noreply.github.com>
2021-10-21 10:44:11 +0300

    Update spiritlevel.js

--- apps/slevel/spiritlevel.js
+++ apps/slevel/spiritlevel.js
@@ -10,21 +10,62 @@ Bangle.on('accel',function(v) {
 
   var d = Math.sqrt(v.x*v.x+v.y*v.y);
   var ang = Math.atan2(d,Math.abs(v.z))*180/Math.PI;
-  
-  g.setColor(1,1,1);
-  g.setFont("6x8",2);
-  g.setFontAlign(0,-1);
-  g.clearRect(60,0,180,16);
-  g.drawString(ang.toFixed(1),120,0);
-  var n = {
-    x:E.clip(120+v.x*256,4,236),
-    y:E.clip(120+v.y*256,4,236)};
-  g.clearRect(old.x-3,old.y-3,old.x+6,old.y+6);
-  g.setColor(1,1,1);
-  g.fillRect(n.x-3,n.y-3,n.x+6,n.y+6);
-  g.setColor(1,0,0);
-  g.drawCircle(120,120,20);
-  g.drawCircle(120,120,60);
-  g.drawCircle(120,120,100);
-  old = n;
 });
+
+Bangle.setLCDPower(1);
+Bangle.setLCDTimeout(0);
+var counter = 0;
+
+function updateScreen() {
+  g.clearRect(0, 250, 250, 150);
+  g.setColor(0xFFFF);
+  g.setFont("Vector",40).setFontAlign(0,0);
+  //g.drawString(Math.floor(counter), g.getWidth()/2, 100);
+  g.drawString(Math.floor(counter), g.getWidth()/2, 200);
+  g.drawString('-', 45, 200);
+  g.drawString('-', 45, 200);
+  //g.drawString('-', 45, 100);
+  g.drawString('+', 190, 200);
+ // g.drawString('+', 185, 100);
+}
+
+
+
+setWatch(() => {
+  counter += 1;
+  if(counter==7){
+    //g.drawImage(require("Storage").read("am.img"),56,27);
+    counter=7;
+  }
+  updateScreen();
+}, BTN1, {repeat:true});
+
+
+setWatch(() => {
+  counter -= 1;
+  g.clear();
+  if(counter==-1){
+    counter=0;}
+  updateScreen();
+}, BTN3, {repeat:true});
+
+if(ang === 180){
+    counter -= 1;
+  g.clear();
+  if(counter==-1){
+    counter=0;}
+  updateScreen();
+}
+
+setWatch(() => {
+  g.clear();
+  counter = 0;
+  updateScreen();
+}, BTN2, {repeat:true});
+
+g.clear(1).setFont("6x8");
+//g.drawString(' counter for twaf', 25, 200);
+
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+updateScreen();
