a32d8eba89cc8f83e27287ea6ea0ececf3af89d4
A7madF <91619179+A7madF@users.noreply.github.com>
2021-10-21 11:11:25 +0300

    Update spiritlevel.js

--- apps/slevel/spiritlevel.js
+++ apps/slevel/spiritlevel.js
@@ -1,4 +1,10 @@
 g.clear();
+Bangle.setLCDPower(1);
+Bangle.setLCDTimeout(0);
+
+var counter = 0;
+
+
 var old = {x:0,y:0};
 Bangle.on('accel',function(v) {
   var max = Math.max(Math.abs(v.x),Math.abs(v.y),Math.abs(v.z));
@@ -11,19 +17,7 @@ Bangle.on('accel',function(v) {
   var d = Math.sqrt(v.x*v.x+v.y*v.y);
   var ang = Math.atan2(d,Math.abs(v.z))*180/Math.PI;
   
-  if(ang > 10 && ang < 21){
-  counter += 1;
-  if(counter==7){
-    //g.drawImage(require("Storage").read("am.img"),56,27);
-    counter=7;
-  }
-  updateScreen();
-}
-});
-
-Bangle.setLCDPower(1);
-Bangle.setLCDTimeout(0);
-var counter = 0;
+  
 
 function updateScreen() {
   g.clearRect(0, 250, 250, 150);
@@ -65,6 +59,18 @@ setWatch(() => {
   counter = 0;
   updateScreen();
 }, BTN2, {repeat:true});
+  
+  if(ang > 10 && ang < 21){
+  counter += 1;
+  if(counter==7){
+    //g.drawImage(require("Storage").read("am.img"),56,27);
+    counter=7;
+  }
+  updateScreen();
+}
+});
+
+
 
 g.clear(1).setFont("6x8");
 //g.drawString(' counter for twaf', 25, 200);
