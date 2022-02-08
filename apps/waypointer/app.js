0ac00d82e804bd4ab5e92f38de88f5f17be17e60
A7madF <91619179+A7madF@users.noreply.github.com>
2022-02-08 21:58:46 +0300

    Update app.js

--- apps/waypointer/app.js
+++ apps/waypointer/app.js
@@ -5,9 +5,9 @@ var pal_bb = new Uint16Array([0x0000,0x07ff],0,1); // black, blue
 // having 3 2 color pallette keeps the memory requirement lower
 var buf1 = Graphics.createArrayBuffer(160,160,1, {msb:true});
 var buf2 = Graphics.createArrayBuffer(80,40,1, {msb:true});
-var arrow_img = require("heatshrink").decompress(atob("lEowIPMjAEDngEDvwED/4DCgP/wAEBgf/4AEBg//8AEBh//+AEBj///AEBn///gEBv///wmCAAImCAAIoBFggE/AkaaEABo="));
+//var arrow_img = require("heatshrink").decompress(atob("lEowIPMjAEDngEDvwED/4DCgP/wAEBgf/4AEBg//8AEBh//+AEBj///AEBn///gEBv///wmCAAImCAAIoBFggE/AkaaEABo="));
 
-function flip1(x,y) {
+/*function flip1(x,y) {
   g.drawImage({width:160,height:160,bpp:1,buffer:buf1.buffer, palette:pal_by},x,y);
   buf1.clear();
 }
@@ -23,7 +23,7 @@ function flip2_bb(x,y) {
 }
 
 
-
+*/
 var candraw = true;
 var wp_bearing = 0;
 var direction = 0;
@@ -62,7 +62,7 @@ function drawCompass(course) {
 }
 */
 
-
+/*
 var heading = 0;
 function newHeading(m,h){ 
     var s = Math.abs(m - h);
@@ -104,7 +104,7 @@ function read_compass() {
   if (direction > 360) direction -= 360;
   drawCompass(direction);
 }
-
+*/
 
 
 var speed = 0;
