4067674cddb1cd12f14c2276dc5cd59ffd9c8134
A7madF <91619179+A7madF@users.noreply.github.com>
2022-02-09 06:18:18 +0300

    Update app.js

--- apps/waypointer/app.js
+++ apps/waypointer/app.js
@@ -7,7 +7,7 @@ var buf1 = Graphics.createArrayBuffer(160,160,1, {msb:true});
 var buf2 = Graphics.createArrayBuffer(80,40,1, {msb:true});
 //var arrow_img = require("heatshrink").decompress(atob("lEowIPMjAEDngEDvwED/4DCgP/wAEBgf/4AEBg//8AEBh//+AEBj///AEBn///gEBv///wmCAAImCAAIoBFggE/AkaaEABo="));
 
-/*function flip1(x,y) {
+function flip1(x,y) {
   g.drawImage({width:160,height:160,bpp:1,buffer:buf1.buffer, palette:pal_by},x,y);
   buf1.clear();
 }
@@ -21,7 +21,7 @@ function flip2_bb(x,y) {
   g.drawImage({width:80,height:40,bpp:1,buffer:buf2.buffer, palette:pal_bb},x,y);
   buf2.clear();
 }
-*/
+
 var candraw = true;
 var wp_bearing = 0;
 var direction = 0;
@@ -44,7 +44,7 @@ function clear_previous() {
   previous.wp_name = '-';
   previous.course = -999;
 }
-/*
+
 function drawCompass(course) {
   if(!candraw) return;
   if (Math.abs(previous.course - course) < 9) return; // reduce number of draws due to compass jitter
