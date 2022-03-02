dc16f49ceec00e0fa1d02c4e2e1de7fe7c9fe440
A7madF <91619179+A7madF@users.noreply.github.com>
2022-03-02 11:20:31 +0300

    Update gesture.js

--- apps/gesture/gesture.js
+++ apps/gesture/gesture.js
@@ -1,5 +1,32 @@
+Bangle.on('aiGesture',(gesture,raw)=>print(gesture,raw));
+var counter = 0;
 Bangle.on('aiGesture',(gesture)=>{
-  E.showMessage(gesture?gesture:"Unknown");
-  setTimeout(()=>g.clear(), 2000);
+//  E.showMessage(gesture);
+
+if(gesture == 'left' ){
+ if(counter < 8){
+    counter = counter + 1;
+    E.showMessage(gesture);
+    g.setColor(0xFFFF);
+    g.setFont("Vector",40).setFontAlign(0,0);
+    g.drawString(Math.floor(counter), g.getWidth()/2, 200);
+  }
+  
+}
+
+if(gesture == 'right'){
+
+  if(counter > 0 ){
+    counter = counter -1;
+    E.showMessage(gesture);
+    g.setColor(0xFFFF);
+    g.setFont("Vector",40).setFontAlign(0,0);
+    g.drawString(Math.floor(counter), g.getWidth()/2, 200);
+  }
+  
+
+
+}
+   
+  setTimeout(()=>g.clear(), 1000);
 });
-E.showMessage("Wave your hands\nto detect\na gesture","Gesture Test");
